ea3e4c2bccbabc07a42c0ea44a5b157c3ae81da4
Priyanka Chaudhari <priyankachaudhari0008@gmail.com>
2026-09-26 16:04:46 +0530

    Add multilingual Unicode normalization test

---
+++ tests/test_unicode_names.py
@@ -0,0 +1,17 @@
+﻿import sys
+sys.path.insert(0, "src")
+
+from preprocessing.name_normalizer import normalize_business_name
+
+
+def test_multilingual_names():
+    examples = [
+        "राम मार्केटिंग प्राइवेट लिमिटेड",
+        "சதர்ன் இண்டஸ்ட்ரீஸ் பிரைவேட் லிமிடெட்",
+        "શક્તિ અર્બન પ્રોડક્ટ્સ પ્રાઇவேટ લિમિટેડ",
+        "ಕರ್ನಾಟಕ",
+        "Dréxkor",
+    ]
+
+    for name in examples:
+        assert normalize_business_name(name) == name.lower()
